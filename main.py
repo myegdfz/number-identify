42611940cb3f51a71dbfcd0448722261fdf51d40
myegdfz <myegdfz@aliyun.com>
2021-01-07 17:23:41 +0800

    add generator and modified the web resources

--- main.py
+++ main.py
@@ -2,35 +2,53 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import numpy as np
 import tensorflow as tf
+import numpy as np
 from flask import Flask, jsonify, render_template, request
-from mnist import model
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
 
-x = tf.placeholder("float", [None, 784])
-sess = tf.Session()
-
-with tf.variable_scope("regression"):
-    _yr, variables = model.regression(x)
-
-saver = tf.train.Saver(variables)
-saver.restore(sess, "mnist/MNIST_data/CkptData/regression.ckpt")
-
-with tf.variable_scope("convolutional"):
-    keep_prob = tf.placeholder("float")
-    _yc, variables = model.convolutional(x, keep_prob)
-
-sess.run(tf.global_variables_initializer())
-saver = tf.train.Saver(variables)
-saver.restore(sess, "mnist/MNIST_data/CkptData/convolutional.ckpt")
+model_name = './mnist/other/models/GAN-CNN.h5'
+base_model = load_model(model_name)
+base_model.summary()
 
 
-def regression(inx):
-    return sess.run(_yr, feed_dict={x: inx}).flatten().tolist()
+# tf.compat.v1.disable_eager_execution()
+# x = tf.compat.v1.placeholder("float", [None, 784])
+# sess = tf.compat.v1.Session()
+#
+# with tf.compat.v1.variable_scope("regression"):
+#     _yr, variables = model.regression(x)
+#
+# saver = tf.compat.v1.train.Saver(variables)
+# saver.restore(sess, "mnist/MNIST_data/CkptData/regression.ckpt")
+#
+# with tf.compat.v1.variable_scope("convolutional"):
+#     keep_prob = tf.compat.v1.placeholder("float")
+#     _yc, variables = model.convolutional(x, keep_prob)
+#
+# sess.run(tf.compat.v1.global_variables_initializer())
+# saver = tf.compat.v1.train.Saver(variables)
+# saver.restore(sess, "mnist/MNIST_data/CkptData/convolutional.ckpt")
+#
+#
+# def regression(inx):
+#     return sess.run(_yr, feed_dict={x: inx}).flatten().tolist()
+#
+#
+# def convolutional(inx):
+#     return sess.run(_yc, feed_dict={x: inx, keep_prob: 1.0}).flatten().tolist()  #
 
 
-def convolutional(inx):
-    return sess.run(_yc, feed_dict={x: inx, keep_prob: 1.0}).flatten().tolist()
+def cgenerator(inx):
+    inx = inx.astype('float32')
+    inx = np.reshape(inx, (1, 28, 28, 1))
+    # return test_gen.get_function(inx)
+    r_test = np.random.sample(inx.shape[0])
+    pred = base_model.predict([inx, r_test])
+    return pred[0], pred[1]
 
 
 app = Flask(__name__)
@@ -38,11 +56,26 @@ app = Flask(__name__)
 
 @app.route('/api/mnist', methods=['post'])
 def mnist():
-    # print(np.array(request.json, dtype=np.uint8))
-    _input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
-    _output_regression = regression(_input)
-    _output_convolutional = convolutional(_input)
-    return jsonify(results=[_output_regression, _output_convolutional])
+    _input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 28, 28)
+    # print(_input)
+    _input[_input <= 0.08] = 0.
+    _input[_input > 0.08] = 1.
+    _output, img = cgenerator(_input)
+
+    plt.figure(figsize=(10, 5), dpi=100)
+    ax = plt.subplot(2, 1, 1)
+    plt.imshow(_input.reshape(28, 28))
+    plt.gray()
+    ax.set_axis_off()
+    ax = plt.subplot(2, 1, 2)
+    plt.title('%s' % np.argmax(_output))
+    plt.imshow(img.reshape(28, 28))
+    plt.gray()
+    ax.set_axis_off()
+    plt.show()
+
+    print("_output", _output[0].tolist())
+    return jsonify(results=[_output[0].tolist()])
 
 
 @app.route('/')
